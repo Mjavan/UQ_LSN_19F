4030542aaa8ab16a511b624e552f65a862376517
Mjavan <48623760+Mjavan@users.noreply.github.com>
2024-03-12 14:11:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# UQ_LSN_19F/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+name = 'UQ_LSN_19F',
+version ='0.1.0',
+packages = find_packages(include=['UQ_LSN_19F','UQ_LSN_19F.*'])
+)
