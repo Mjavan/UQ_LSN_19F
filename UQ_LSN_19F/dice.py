e52920ba8c440b2c92352a58f00fdcb705295284
Mjavan <48623760+Mjavan@users.noreply.github.com>
2024-10-25 11:39:03 +0200

    Update dice.py

--- UQ_LSN_19F/dice.py
+++ UQ_LSN_19F/dice.py
@@ -7,49 +7,35 @@ from torch import nn
 
 class DiceCoef(nn.Module):
     def __init__(self):
-
         super(DiceCoef,self).__init__()
 
     def forward(self,pred,target,smooth=1.):
-        
         if isinstance(pred,np.ndarray):
             pred =torch.from_numpy(pred)
-            
         if isinstance(target,np.ndarray):
             target = torch.from_numpy(target)
-        
+            
         iflat = pred.contiguous().view(-1)
         tflat = target.contiguous().view(-1)
-
         intersection = (iflat * tflat).sum()
-
         A_sum = torch.sum(iflat * iflat)
         B_sum = torch.sum(tflat * tflat)
-
         score =(2. * intersection + smooth) / (A_sum + B_sum + smooth)
-        
         return(score)
     
 
 
 class DiceLoss(nn.Module):
-
     def __init__(self):
-
         super(DiceLoss,self).__init__()
 
     def forward(self,pred,target,smooth=1.):
-        
         iflat = pred.contiguous().view(-1)
         tflat = target.contiguous().view(-1)
-
         intersection = (iflat * tflat).sum()
-
         A_sum = torch.sum(iflat * iflat)
         B_sum = torch.sum(tflat * tflat)
-
         score =(2. * intersection + smooth) / (A_sum + B_sum + smooth)
         loss = 1-score
-
         return(loss)
     
